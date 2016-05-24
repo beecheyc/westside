a458f6dfafdbf4bde642fedcca376b81202fbb51
beecheyc <beecheyc@gmail.com>
2016-05-24 15:57:34 +1000

    Initial commit

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Gypsy brewery based out of West Melbourne">
+    <meta name="author" content="Marble Fox Design">
+    <meta name="keywords" content="Craft Beer, Sunshine, Ale, Australia, Beer, Footscray, West Melbourne">
+    <link rel="icon" href="IMGS/favicon.ico">
+
+    <title>WEST CITY BREWING</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--my style sheet here -->  
+    <link href="CSS/style.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            <a class="navbar-brand" href="#">Ales from the West</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a class="scroll" href="#home">Home</a></li>
+                <li><a class="scroll" href="#beer">Beers</a></li>
+                <li><a class="scroll" href="#about">About</a></li>
+                <li><a class="scroll" href="#contact">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+
+
+  
+     <img src="header1.gif" class="img-responsive" alt="Responsive image">
+  
+
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+    <a id="beer"></a>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle" src="IMGS/footscraysml.png" alt="footscray" width="250" height="250">
+          <h2>Footscray Pale Ale - 4.3%</h2>
+          <p class="beer">A thirst quenching, highly drinkable pale ale made with all American hops and New Zealand malts. A decent hit of biscuit malt gives the beer a full flavour that remains easy to drink.</p>
+          <!--<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>-->
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="IMGS/oatysml.png" alt="oatmeal" width="250" height="250">
+          <h2>Oaty Session Stout - 5.2%</h2>
+          <p class="beer">We like to call this our ‘Session Stout’ for its easy drinking qualities! Brewed as a traditional style Oatmeal Stout with drinkability in mind. Light roasty flavours, a nice mouthfeel, with a subtle dry finish.</p>
+          <!--<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>-->
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="IMGS/sunshinesml.png" alt="sunshine" width="250" height="250">
+          <h2>Sunshine Ale - 4.8%</h2>
+          <p class="beer">Our Sunshine Ale is based on a simple Blonde Ale recipe, using lightly kilned malts, and only a single hop, Mandarina Bavaria. A very clean simple summer ale.</p>
+         <!-- <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>-->
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+        </div>
+        
+    
+
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+        <a id="about"></a>
+        <div class="container">
+      <div class="row featurette">
+        <div class="col-md-7">
+            <h2 class="about_headers">West City Brewing.<br></h2> <h3><span class="text-muted">Our story...</span></h3>
+          <p class="about">West Melbourne locals and passionate home brewers Bevan and Fergus began discussing their brewing techniques and recipies over the Facebook page "West Side Beer Drinkers". Both coming from a "tradie" background and obvious beer lovers, they decided to collaborate. The result...West City Brewing, their aim is to create approachable sessionable beers, with a West Melbourne influence. Currently gypsy brewing out of various Melbourne breweries, they're on the hunt for their own piece of land to set up shop. </p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" img src="IMGS/boys.jpg" alt="The Boys">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+            <h2 class="about_headers">We're on tap here.. <br></h2><h4><span class="text-muted">Venues currently hosting our taps.</span></h4>
+          <!--<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p> -->
+            <iframe src='http://nowtapped.com/embed.html?brewery=222732&columns=2' style='border: 0; width:100%; height:500px;'></iframe>
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" img src="IMGS/dogtap.jpg" alt="Tap">
+        </div>
+          <!--<p class="pull-right"><a class="scroll" href="#home">Back to top</a></p> -->
+      </div>
+
+      <hr class="featurette-divider">
+
+     <!-- <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div> -->
+        </div>
+     <!-- <hr class="featurette-divider"> -->
+
+      <!-- /END THE FEATURETTES -->
+        <!----------- CONTACT FORM BELOW -------->
+        <a id="contact"></a>
+        <div class="container">
+            <h3 class="contact-head">Contact Us</h3>
+        <form class="form-horizontal" name="contact_form" method="post" action="postemail.php">
+            <fieldset>
+                <legend class="legend">Complete the form below</legend> 
+                    
+                <div class="form-group">
+                    <label for="Name">Name*</label>
+                    <input type="text" class="form-control" name="Name" id="Name" placeholder="Enter name" autofocus="autofocus" required />
+                </div>
+                <div class="form-group">
+                    <label for="Email">Email*</label>
+                    <input type="email" class="form-control" name="Email" id="email" placeholder="Enter email" required />
+                </div>
+                <div class="form-group">
+                    <label for="Phone">Phone</label>
+                    <input type="text" class="form-control" name="number" id="number" placeholder="" />
+                </div>
+                <div class="form-group">                    
+                    <label for="Message">Message*</label>
+                    <textarea name="message" class="form-control" id="message" cols="60" rows="5" placeholder="Enter message.." required></textarea>
+                </div>
+                    
+
+                        
+                        <input type="reset" name="reset" id="reset" value="Reset" />
+                        <input type="submit" name="send" id="send" value="Submit" />
+
+            
+            </fieldset>
+        </form> 
+        </div>
+        </div>
+        
+        <!----------CONTACT FORM ABOVE ------------>
+
+        <hr class="featurette-divider"> 
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a class="scroll" href="#home">Back to top</a></p>
+        <p>&copy; 2016 West City Brewing.  &middot; Website by <a href="#" title="COMING SOON">Marble Fox - Design.</a> <!--<a href="#">Privacy</a> &middot; <a href="#">Terms</a>--></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+
+    <script src="JS/jquery-1.12.0.min.js"></script>
+    <script src="JS/scroll.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
